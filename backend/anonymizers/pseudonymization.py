eb3b646d37cdacfeb00066791c1107c7c7f72f44
aware37 <awareaware37@gmail.com>
2025-04-04 23:31:58 +0200

    modif back et front

--- backend/anonymizers/pseudonymization.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-
-def apply_pseudonymization(df, column):
-    def pseudonymize(value):
-        value = str(value)
-        return hashlib.sha256(value.encode('utf-8')).hexdigest()[:16]
-
-    df[column] = df[column].apply(pseudonymize)
-    return df
